511f3b50499c13166efe106ec1b924a4fa321547
Creator69 <kreponic.e@gmail.com>
2022-04-12 00:34:44 +0200

    create test on discount model

--- discounts/tests.py
+++ discounts/tests.py
@@ -1,10 +1,29 @@
 from django.test import TestCase
+from .models import Discount
+
+import datetime
 
 
 class TestModels(TestCase):
-    pass
+    def setUp(self):
+        Discount.objects.create(brand_name="TEST_BRAND",
+                                discount_percentage=10,
+                                number_of_codes=50,
+                                enabled=1,
+                                valid_from=datetime.datetime(2022, 4, 11, 7, 29, 33, tzinfo=datetime.timezone.utc),
+                                valid_to=datetime.datetime(2023, 4, 11, 7, 29, 33, tzinfo=datetime.timezone.utc)
+                                )
+
+    def test_discount_model(self):
+        discount = Discount.objects.get(brand_name="TEST_BRAND")
+        self.assertEqual(discount.brand_name, "TEST_BRAND")
+        self.assertEqual(discount.discount_percentage, 10)
+        self.assertEqual(discount.number_of_codes, 50)
+        self.assertEqual(discount.enabled, 1)
+        self.assertEqual(discount.valid_from, datetime.datetime(2022, 4, 11, 7, 29, 33, tzinfo=datetime.timezone.utc))
+        self.assertEqual(discount.valid_to, datetime.datetime(2023, 4, 11, 7, 29, 33, tzinfo=datetime.timezone.utc))
 
 
 class TestViews(TestCase):
-    pass
-    ##
+    def test_get_discount(self):
+        pass
